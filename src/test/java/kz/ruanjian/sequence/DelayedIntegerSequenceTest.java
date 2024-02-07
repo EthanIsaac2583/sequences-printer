54ea1a7328133cd24fd1c23b418c0a918703893c
eitan.aizek <eitan.aizek@techork.com>
2024-02-07 18:18:07 +0500

    sp: fixed versions

--- src/test/java/kz/ruanjian/sequence/DelayedIntegerSequenceTest.java
+++ src/test/java/kz/ruanjian/sequence/DelayedIntegerSequenceTest.java
@@ -1,11 +1,16 @@
 package kz.ruanjian.sequence;
 
 import kz.ruanjian.threaded.SafeRandom;
+import kz.ruanjian.threaded.SafeSleeper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class DelayedIntegerSequenceTest {
@@ -14,13 +19,24 @@ class DelayedIntegerSequenceTest {
     SafeRandom random;
 
     @Mock
+    SafeSleeper sleeper;
+
+    @Mock
     IntegerSequence mockedSequence;
 
     @InjectMocks
     DelayedIntegerSequence delayedIntegerSequence;
 
     @Test
-    void generate() {
+    void generate_shouldDoAppropriateActions_whenInvoked() {
+        int expectedSleepMillis = 1234;
+        int expected = 10;
+        doReturn(expectedSleepMillis).when(random).get();
+        doReturn(expected).when(mockedSequence).generate();
 
+        int actual = delayedIntegerSequence.generate();
+
+        assertEquals(expected, actual);
+        verify(sleeper).sleep(expectedSleepMillis);
     }
 }
