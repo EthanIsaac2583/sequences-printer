494a4bc8ad64e2d6eac8dc51d0a9efcb47681e93
eitan.aizek <eitan.aizek@techork.com>
2024-02-08 01:56:06 +0500

    sp: added SleepingRunnerTest

---
+++ src/test/java/kz/ruanjian/SleepingRunnerTest.java
@@ -0,0 +1,56 @@
+package kz.ruanjian;
+
+import kz.ruanjian.loopcontrol.LoopControl;
+import kz.ruanjian.threaded.SafeRandom;
+import kz.ruanjian.threaded.SafeSleeper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.inOrder;
+
+@ExtendWith(MockitoExtension.class)
+class SleepingRunnerTest {
+
+    @Mock
+    Runnable runnable;
+
+    @Mock
+    LoopControl loopControl;
+
+    @Mock
+    SafeSleeper sleeper;
+
+    @Mock
+    SafeRandom random;
+
+    @InjectMocks
+    SleepingRunner sleepingRunner;
+
+    @Test
+    void run() {
+        doReturn(true).doReturn(true).doReturn(true).doReturn(false).when(loopControl).canExecute();
+        doReturn(100).doReturn(200).doReturn(300).when(random).get();
+
+        InOrder inOrder = inOrder(sleeper, runnable, random, loopControl);
+
+        sleepingRunner.run();
+
+        inOrder.verify(loopControl).canExecute();
+        inOrder.verify(random).get();
+        inOrder.verify(sleeper).sleep(100);
+        inOrder.verify(runnable).run();
+        inOrder.verify(loopControl).canExecute();
+        inOrder.verify(random).get();
+        inOrder.verify(sleeper).sleep(200);
+        inOrder.verify(runnable).run();
+        inOrder.verify(loopControl).canExecute();
+        inOrder.verify(random).get();
+        inOrder.verify(sleeper).sleep(300);
+        inOrder.verify(runnable).run();
+    }
+}
