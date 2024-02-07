15c1f9ed388c2c62e02bbcb9a1075e68a9b36336
eitan.aizek <eitan.aizek@techork.com>
2024-02-07 18:13:09 +0500

    sp: fixed DelayedIntegerSequence

--- src/main/java/kz/ruanjian/sequence/DelayedIntegerSequence.java
+++ src/main/java/kz/ruanjian/sequence/DelayedIntegerSequence.java
@@ -1,28 +1,25 @@
 package kz.ruanjian.sequence;
 
 import kz.ruanjian.threaded.SafeRandom;
+import kz.ruanjian.threaded.SafeSleeper;
 
 public class DelayedIntegerSequence implements IntegerSequence {
 
     private final SafeRandom random;
+    private final SafeSleeper sleeper;
     private final IntegerSequence sequence;
 
-    public DelayedIntegerSequence(SafeRandom random, IntegerSequence sequence) {
+    public DelayedIntegerSequence(SafeRandom random,
+                                  SafeSleeper sleeper,
+                                  IntegerSequence sequence) {
         this.random = random;
+        this.sleeper = sleeper;
         this.sequence = sequence;
     }
 
     @Override
     public Integer generate() {
-        trySleep();
+        sleeper.sleep(random.get());
         return sequence.generate();
-    }
-
-    private void trySleep() {
-        try {
-            Thread.sleep(random.get());
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
     }
 }
