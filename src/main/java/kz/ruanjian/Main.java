bd30bdc0454401f91d810aa01385a516527d64e4
eitan.aizek <eitan.aizek@techork.com>
2024-02-07 21:45:37 +0500

    sp: changed Printer

--- src/main/java/kz/ruanjian/Main.java
+++ src/main/java/kz/ruanjian/Main.java
@@ -12,9 +12,6 @@ import kz.ruanjian.threaded.SafeSleeper;
 
 import java.util.Deque;
 import java.util.concurrent.ConcurrentLinkedDeque;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
 
@@ -24,13 +21,14 @@ public class Main {
         SafeSleeper sleeper = new SafeSleeper();
         Logger logger = new ConsoleLogger();
         CountLoopControl infiniteCountLoopControl = new CountLoopControl(-1);
+
         IntegerSequence delayedArithmeticThrees = new DelayedIntegerSequence(new SafeRandom(200, 300), sleeper, new ArithmeticIntegerSequence(3, 3));
         IntegerSequence delayedArithmeticFives = new DelayedIntegerSequence(new SafeRandom(2000, 2500), sleeper, new ArithmeticIntegerSequence(5, 5));
 
+        DurationLoopControl printDurationControl = new DurationLoopControl(new SafeRandom(200, 500).get());
+
         new Thread(new Producer(stack, delayedArithmeticThrees, infiniteCountLoopControl, logger)).start();
         new Thread(new Producer(stack, delayedArithmeticFives, infiniteCountLoopControl, logger)).start();
-
-        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-        executor.scheduleAtFixedRate(new Printer(stack, new DurationLoopControl(new SafeRandom(200, 500).get()), logger), new SafeRandom(4, 8).get(), new SafeRandom(15, 20).get(), TimeUnit.SECONDS);
+        new Thread(new Printer(stack, printDurationControl, infiniteCountLoopControl, sleeper, logger)).start();
     }
 }
--- src/main/java/kz/ruanjian/Printer.java
+++ src/main/java/kz/ruanjian/Printer.java
@@ -1,33 +1,50 @@
 package kz.ruanjian;
 
 import kz.ruanjian.logger.Logger;
+import kz.ruanjian.loopcontrol.CountLoopControl;
 import kz.ruanjian.loopcontrol.DurationLoopControl;
+import kz.ruanjian.threaded.SafeRandom;
+import kz.ruanjian.threaded.SafeSleeper;
 
 import java.util.Deque;
 
 public class Printer implements Runnable {
 
     private final Deque<Integer> stack;
-    private final DurationLoopControl loopControl;
+    private final DurationLoopControl durationLoopControl;
+    private final CountLoopControl countLoopControl;
+    private final SafeSleeper sleeper;
     private final Logger logger;
 
-    public Printer(Deque<Integer> stack, DurationLoopControl loopControl, Logger logger) {
+    public Printer(Deque<Integer> stack,
+                   DurationLoopControl durationLoopControl,
+                   CountLoopControl countLoopControl,
+                   SafeSleeper sleeper,
+                   Logger logger) {
         this.stack = stack;
-        this.loopControl = loopControl;
+        this.durationLoopControl = durationLoopControl;
+        this.countLoopControl = countLoopControl;
+        this.sleeper = sleeper;
         this.logger = logger;
     }
 
     @Override
     public void run() {
-        loopControl.setStartMillis(System.currentTimeMillis());
-        logger.log("------- [PRINT WINDOW] opened -------");
-        while (loopControl.canExecute()) {
-            Integer polled = stack.poll();
+        while (countLoopControl.canExecute()) {
+            sleeper.sleep(new SafeRandom(4000, 8000).get());
 
-            if (polled != null) {
-                logger.log("Printed: " + polled);
+            logger.log("------- [PRINT WINDOW] opened -------");
+
+            durationLoopControl.setStartMillis(System.currentTimeMillis());
+            while (durationLoopControl.canExecute()) {
+                Integer polled = stack.poll();
+
+                if (polled != null) {
+                    logger.log("Printed: " + polled);
+                }
             }
+
+            logger.log("------- [PRINT WINDOW] closed -------");
         }
-        logger.log("------- [PRINT WINDOW] closed -------");
     }
 }
