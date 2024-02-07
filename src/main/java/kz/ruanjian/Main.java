99101dabaab474f17cfee46d65e5f1df0a12d196
eitan.aizek <eitan.aizek@techork.com>
2024-02-07 20:45:08 +0500

    sp: used Producer threads

--- src/main/java/kz/ruanjian/Main.java
+++ src/main/java/kz/ruanjian/Main.java
@@ -2,12 +2,28 @@ package kz.ruanjian;
 
 import kz.ruanjian.logger.ConsoleLogger;
 import kz.ruanjian.logger.Logger;
+import kz.ruanjian.loopcontrol.CountLoopControl;
+import kz.ruanjian.sequence.ArithmeticIntegerSequence;
+import kz.ruanjian.sequence.DelayedIntegerSequence;
+import kz.ruanjian.sequence.IntegerSequence;
+import kz.ruanjian.threaded.SafeRandom;
+import kz.ruanjian.threaded.SafeSleeper;
+
+import java.util.Deque;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class Main {
 
     public static void main(String[] args) {
-        Logger logger = new ConsoleLogger();
+        Deque<Integer> stack = new ConcurrentLinkedDeque<>();
 
-        logger.log("Hi!");
+        SafeSleeper sleeper = new SafeSleeper();
+        Logger logger = new ConsoleLogger();
+        CountLoopControl infiniteCountLoopControl = new CountLoopControl(-1);
+        IntegerSequence delayedArithmeticThrees = new DelayedIntegerSequence(new SafeRandom(500, 700), sleeper, new ArithmeticIntegerSequence(3, 3));
+        IntegerSequence delayedArithmeticFives = new DelayedIntegerSequence(new SafeRandom(2000, 3000), sleeper, new ArithmeticIntegerSequence(5, 5));
+
+        new Thread(new Producer(stack, delayedArithmeticThrees, infiniteCountLoopControl, logger)).start();
+        new Thread(new Producer(stack, delayedArithmeticFives, infiniteCountLoopControl, logger)).start();
     }
 }
