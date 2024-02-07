28bf4fa5f2adfe4dae3c1f6cdaf23515259c8760
eitan.aizek <eitan.aizek@techork.com>
2024-02-08 02:34:07 +0500

    sp: added OncePerThread

---
+++ src/main/java/kz/ruanjian/OncePerThread.java
@@ -0,0 +1,4 @@
+package kz.ruanjian;
+
+public interface OncePerThread {
+}
--- src/main/java/kz/ruanjian/loopcontrol/DurationLoopControl.java
+++ src/main/java/kz/ruanjian/loopcontrol/DurationLoopControl.java
@@ -1,28 +1,27 @@
 package kz.ruanjian.loopcontrol;
 
+import kz.ruanjian.OncePerThread;
 import kz.ruanjian.threaded.SafeRandom;
 
-import java.util.concurrent.atomic.AtomicLong;
-
-public class DurationLoopControl implements LoopControl {
+public class DurationLoopControl implements LoopControl, OncePerThread {
 
     private final int durationMinMillis;
     private final int durationMaxMillis;
-    private final AtomicLong duration = new AtomicLong(0L);
-    private final AtomicLong startMillis = new AtomicLong(0L);
+    private long duration;
+    private long startMillis;
 
     public DurationLoopControl(int durationMinMillis, int durationMaxMillis) {
         this.durationMinMillis = durationMinMillis;
         this.durationMaxMillis = durationMaxMillis;
     }
 
-    public synchronized void pointCurrentTime() {
-        this.startMillis.set(System.currentTimeMillis());
-        this.duration.set(new SafeRandom(durationMinMillis, durationMaxMillis).get());
+    public void pointCurrentTime() {
+        this.startMillis = System.currentTimeMillis();
+        this.duration = new SafeRandom(durationMinMillis, durationMaxMillis).get();
     }
 
     @Override
-    public synchronized boolean canExecute() {
-        return System.currentTimeMillis() - startMillis.get() <= duration.get();
+    public boolean canExecute() {
+        return System.currentTimeMillis() - startMillis <= duration;
     }
 }
