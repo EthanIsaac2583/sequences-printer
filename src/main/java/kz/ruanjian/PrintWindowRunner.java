cf4b03cffc4efc6d7a07a1b612d56dbe067f0a92
eitan.aizek <eitan.aizek@techork.com>
2024-02-09 00:38:02 +0500

    sp: added PrintWindowRunner

---
+++ src/main/java/kz/ruanjian/PrintWindowRunner.java
@@ -0,0 +1,40 @@
+package kz.ruanjian;
+
+import kz.ruanjian.formatter.PrinterFormatter;
+import kz.ruanjian.logger.Logger;
+import kz.ruanjian.loopcontrol.DurationLoopControl;
+
+public class PrintWindowRunner implements Runnable {
+
+    private final Runnable runnable;
+    private final DurationLoopControl loopControl;
+    private final Logger logger;
+    private final PrinterFormatter formatter;
+
+    public PrintWindowRunner(Runnable runnable,
+                             DurationLoopControl loopControl,
+                             Logger logger,
+                             PrinterFormatter formatter) {
+        this.runnable = runnable;
+        this.loopControl = loopControl;
+        this.logger = logger;
+        this.formatter = formatter;
+    }
+
+    @Override
+    public void run() {
+        logger.log(formatter.timedMessage("[PRINTER]", "------- [PRINT WINDOW] opened -------"));
+        long startMillis = System.currentTimeMillis();
+
+        loopControl.fromNow();
+        while (loopControl.canExecute()) {
+            if (runnable != null) {
+                runnable.run();
+            }
+        }
+
+        long duration = System.currentTimeMillis() - startMillis;
+        logger.log(formatter.timedMessage("[PRINTER]: ", "------- [PRINT WINDOW] closed -------"));
+        logger.log(formatter.timedMessage("[PRINTER]: ", "Print duration " + duration + " millis"));
+    }
+}
--- src/main/java/kz/ruanjian/Printer.java
+++ src/main/java/kz/ruanjian/Printer.java
@@ -2,43 +2,29 @@ package kz.ruanjian;
 
 import kz.ruanjian.formatter.PrinterFormatter;
 import kz.ruanjian.logger.Logger;
-import kz.ruanjian.loopcontrol.DurationLoopControl;
 
 import java.util.Deque;
 
 public class Printer implements Runnable {
 
     private final Deque<Integer> stack;
-    private final DurationLoopControl durationLoopControl;
     private final Logger logger;
     private final PrinterFormatter formatter;
 
     public Printer(Deque<Integer> stack,
-                   DurationLoopControl durationLoopControl,
                    Logger logger,
                    PrinterFormatter formatter) {
         this.stack = stack;
-        this.durationLoopControl = durationLoopControl;
         this.logger = logger;
         this.formatter = formatter;
     }
 
     @Override
     public void run() {
-        logger.log(formatter.timedMessage("[PRINTER]", "------- [PRINT WINDOW] opened -------"));
-        Long startMillis = System.currentTimeMillis();
-
-        durationLoopControl.fromNow();
-        while (durationLoopControl.canExecute()) {
-            if (!stack.isEmpty()) {
-                logger.log(formatter.timedMessage("[STACK]", stack));
-                Integer polled = stack.poll();
-                logger.log(formatter.timedMessage("[Printed]", polled));
-            }
+        if (!stack.isEmpty()) {
+            logger.log(formatter.timedMessage("[STACK]", stack));
+            Integer polled = stack.poll();
+            logger.log(formatter.timedMessage("[Printed]", polled));
         }
-
-        Long endMillis = System.currentTimeMillis();
-        logger.log(formatter.timedMessage("[PRINTER]: ", "------- [PRINT WINDOW] closed -------"));
-        logger.log(formatter.timedMessage("[PRINTER]: ", "Print duration " + (endMillis - startMillis) + " millis"));
     }
 }
